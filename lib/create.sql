3872a655f9cf40ef65dcbd937e6ae60acd614d44
Carrie Savinell <carrielane89@gmail.com>
2018-06-27 16:48:36 +0000

    automatically backed up by learn

--- lib/create.sql
+++ lib/create.sql
@@ -15,5 +15,17 @@ CREATE TABLE users (
 
 CREATE TABLE pledges (   #belongs to a user; belongs to a project
   id INTEGER PRIMARY KEY,
-  amount REAL
+  amount REAL,
+  project_id INTEGER,
+  user_id INTEGER
+);
+
+CREATE TABLE projects_pledges (
+  project_id INTEGER,
+  pledge_id INTEGER
+);
+
+CREATE TABLE users_pledges (
+  user_id INTEGER,
+  pledge_id INTEGER,
 );
